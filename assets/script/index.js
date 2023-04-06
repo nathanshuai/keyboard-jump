93fa8e67e47b2c876157c3dbd062f475b5448216
nathanshuai <nathan.shuai@gmail.com>
2023-04-05 21:48:54 -0500

    high score

--- assets/script/index.js
+++ assets/script/index.js
@@ -5,7 +5,7 @@
 */
 
 
-const figure = document.querySelector(".minions");
+const figure = document.querySelector('.minions');
 const center = document.querySelector(".center");
 const playGround = document.querySelector('.playground');
 
@@ -85,31 +85,31 @@ const scoreDisplay = document.getElementById('score');
 const dateDisplay = document.getElementById('date');
 const percentageDisplay = document.getElementById('percentage');
 
-const scores = [];
+const dialog = document.querySelector('dialog');
+const scoreTable = document.querySelector('dialog table tbody');
+
+
 
 submitButton.addEventListener('click', submitWord);
 wordInput.addEventListener('keypress', handleKeyPress);
 restartButton.addEventListener('click', reStartGame);
 
+
+dialog.addEventListener('click', function(e) {
+  const rect = this.getBoundingClientRect();
+
+  if (e.clientY < rect.top || e.clientY > rect.bottom ||
+      e.clientX < rect.left || e.clientX > rect.right){
+        dialog.close();
+      }
+});
+
+
 // Function to start game
 function startGame() {
   figure.remove();
   center.remove();
-  hitsSound.play();
-  backgroundSound.play();
-  playGround.classList.remove('playground');
-  points = 0;
-  timeLeft = 99;
-  randomWord = getRandomWord();
-  message.textContent = '';
-  wordElement.textContent = randomWord;
-  pointsDisplay.textContent = `Points: ${points}`;
-  timeLeftDisplay.textContent = ` ${timeLeft} `;
-  wordInput.value = '';
-  wordInput.disabled = false;
-  submitButton.disabled = false;
-  resultContainer.classList.add('hidden');
-  gameTimer = setInterval(updateTimer, 1000);
+  reStartGame();
 }  
 
 function reStartGame() {
@@ -117,7 +117,7 @@ function reStartGame() {
   backgroundSound.play();
   playGround.classList.remove('playground');
   points = 0;
-  timeLeft = 99;
+  timeLeft = 9;
   randomWord = getRandomWord();
   message.textContent = '';
   wordElement.textContent = randomWord;
@@ -128,6 +128,7 @@ function reStartGame() {
   submitButton.disabled = false;
   resultContainer.classList.add('hidden');
   gameTimer = setInterval(updateTimer, 1000);
+  dialog.close();
 }  
 
 function submitWord() {
@@ -162,28 +163,86 @@ function updateTimer() {
   }
 }
 
+function getRandomWord() {
+  return words[Math.floor(Math.random() * words.length)];
+}
+
+
 function endGame() {
   finishSound.play();
   clearInterval(gameTimer);
   wordInput.disabled = true;
   submitButton.disabled = true;
   scoreDisplay.textContent = points;
+  dialog.innerHTML = '<h3>High Score</h3>';
   resultContainer.classList.remove('hidden');
   playGround.classList.add('playground');
-  let total = 90
-  const date = new Date();
-  const score = new Score(date, points, total);
-  scores.push(score);
-  
+
+  let scores = [];
+  const score = new Score(new Date(), points, 90);
+
   scoreDisplay.textContent = `You points: ${score.points}`;
   percentageDisplay.textContent = `Percentage: ${score.percentage} %`;
   dateDisplay.textContent = `Date: ${score.date.toString().substring(3, 15)}`;
+  
+  const scoreObject = {
+    hits: points,
+    percentage: Math.round((points / 90) * 100)
+  };
 
+  scores.push(scoreObject);
+  scores.sort((a, b) => b.hits - a.hits);
+  scores.splice(9);
+  const toString = JSON.stringify(scores)
+  localStorage.setItem('scores', toString);
+  displayScores();
+}  
+
+function displayScores() {
+  const storedScores = JSON.parse(localStorage.getItem('scores'));
+
+  // If there are no scores, display a message
+  if (!storedScores || storedScores.length === 0) {
+    console.log('No scores to display.');
+    return;
+  }
+
+  const table = document.createElement('table');
+
+  const headerRow = document.createElement('tr');
+  const rankHeader = document.createElement('th');
+  const hitsHeader = document.createElement('th');
+  const percentageHeader = document.createElement('th');
+  rankHeader.textContent = 'Rank';
+  hitsHeader.textContent = 'Hits';
+  percentageHeader.textContent = 'Percentage';
+  headerRow.appendChild(rankHeader);
+  headerRow.appendChild(hitsHeader);
+  headerRow.appendChild(percentageHeader);
+  table.appendChild(headerRow);
+
+  // Create a row for each score
+  storedScores.forEach((score, index) => {
+    const row = document.createElement('tr');
+    const rankCell = document.createElement('td');
+    const hitsCell = document.createElement('td');
+    const percentageCell = document.createElement('td');
+    rankCell.textContent = ` No.${index + 1}`;
+    hitsCell.textContent = score.hits;
+    percentageCell.textContent = `${score.percentage} %`;
+    row.appendChild(rankCell);
+    row.appendChild(hitsCell);
+    row.appendChild(percentageCell);
+    table.appendChild(row);
+  });
+
+  // Display the table in a dialog
+  const dialog = document.querySelector('dialog');
+  dialog.appendChild(table);
+  dialog.showModal();
 }
 
-function getRandomWord() {
-  return words[Math.floor(Math.random() * words.length)];
-}
+
 
 
 
